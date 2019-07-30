c4796e56c631a679a4f76343dcd807e97be2f52a
Livan Jimenez <jimenez.livan14@gmail.com>
2019-07-30 17:19:02 -0400

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callBackend()
+      .then(res => this.setState({ data: res.myBackend }))
+      .catch(err => console.log(err));
+  }
+
+  callBackend = async () => {
+    const response = await fetch('/backend');
+    const body = await response.json();
+
+    if (response.status !== 200)
+      throw Error(body.message);
+
+    return body;
+  };
+
+
+  return() {
+    return (
+      <div>
+        <p>{this.state.data}</p>
+      </div>
+    );
+
+  }
 }
+
+const backendStyle = {
+  fontSize: '24px',
+  textAlign: 'center'
+};
 
 export default App;
