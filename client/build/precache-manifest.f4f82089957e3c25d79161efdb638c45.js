64d69a3b34d4f925fe5951c805eec27309b3399f
Livan Jimenez <jimenez.livan14@gmail.com>
2019-08-04 19:14:06 -0400

    build

---
+++ client/build/precache-manifest.f4f82089957e3c25d79161efdb638c45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32beafbeaea9eeb1e5412c64159076a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c560962ad06cf75b2fd",
+    "url": "/static/css/main.0151ce3c.chunk.css"
+  },
+  {
+    "revision": "da430daded4c46d5662e",
+    "url": "/static/js/2.04fe486e.chunk.js"
+  },
+  {
+    "revision": "0c560962ad06cf75b2fd",
+    "url": "/static/js/main.4505bdfe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
